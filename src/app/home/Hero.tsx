fe62904a4639e6414ecd31bfe8e5a66900bc67b0
mgavillo <marie.gavillon@gmail.com>
2024-10-24 20:10:53 +0400

    update title and subtitle

--- src/app/home/Hero.tsx
+++ src/app/home/Hero.tsx
@@ -5,14 +5,17 @@ import Image from "next/image";
 export default function Hero() {
     return (
         <section className="h-screen flex flex-col-reverse md:flex-row w-full gap-8 md:gap-12 items-center px-4 sm:px-6 lg:px-16 xl:px-32 relative py-20 md:py-0">
-
-            <div className="flex-1 flex flex-col items-start gap-8 md:gap-12 flex-grow min-w-60 max-w-2/3 z-10">
-                <ThreeHeaders title="Transform your life, one SOL at a time!" subTitle="Ascend" paragraph="Build good habits in a fun and social way while insure you get punished if you don't commit. Private beta testing is opening soon. Join the community to get invited and receive updates!" gradient="bg-gradient-to-tr from-[#8D47FF] to-[#B86CF4] bg-clip-text text-transparent" />
+            <div className="flex-1 flex flex-col items-start gap-8 md:gap-12 flex-grow min-w-60 max-w-2/3 max-w-[800px] z-10">
+                <ThreeHeaders
+                    subTitle="Ascend"
+                    title="Hack your mind"
+                    paragraph="A social network designed to help you build better habits through competition, social pressure, and monetary incentives."
+                    gradient="bg-gradient-to-tr from-[#8D47FF] to-[#B86CF4] bg-clip-text text-transparent" />
                 <a href="https://discord.gg/ymVPRnpuqC" target="_blank" rel="noreferrer" className="  text-white bg-gradient-to-r from-[#6663FF] to-[#7F00FE] rounded-full px-6 py-2 hover:opacity-80 transition-opacity cursor-pointer">
                     Join Discord
                 </a>
             </div>
-            <div className="flex-1 w-full -mb-20 md:mb-0 md:-mx-36 lg:-mx-36 lg:scale-125">
+            <div className="flex-1 w-full -mb-20 md:mb-0 md:-mx-36 lg:-mx-36 lg:scale-125 pointer-events-none">
                 <Image src="/shots.png" alt="phone mock up" width={1000} height={2000} className=" h-full object-cover" />
             </div>
             <div className="absolute inset-0 z-50 pointer-events-none">
